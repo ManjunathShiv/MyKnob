fd9485439e4bfd47a32c01b0ed5586ed9d94e953
ManjunathShiv <32102571+ManjunathShiv@users.noreply.github.com>
2019-07-03 09:59:50 +0530

    Initial Commit

---
+++ MyKnob/MyKnob/MyKnob.h
@@ -0,0 +1,19 @@
+//
+//  MyKnob.h
+//  MyKnob
+//
+//  Created by Manjunath Shivakumara on 02/07/19.
+//  Copyright © 2019 Manjunath Shivakumara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyKnob.
+FOUNDATION_EXPORT double MyKnobVersionNumber;
+
+//! Project version string for MyKnob.
+FOUNDATION_EXPORT const unsigned char MyKnobVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyKnob/PublicHeader.h>
+
+
